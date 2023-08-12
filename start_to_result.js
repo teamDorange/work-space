3ced66f843efa1c0553bc8077cbdd081f0eb3968
sema <furuse_yuta@icloud.com>
2023-08-12 12:02:35 +0900

    ランダムに結果が変わるように編集しました。加えて結果が下に寄って表示されてしまっていたので修正しました。ブランチ作り忘れていました。すみません

--- start_to_result.js
+++ start_to_result.js
@@ -1,3 +1,8 @@
+//おみくじ結果のデータセット
+const results = [["大吉","今日は最高の一日になります！"],["中吉","今日はいいことがあるでしょう"],["吉","なにかいいことがあるかも"],["末吉","良くも悪くもなし！"],["凶","何か悪いことがあるので気を付けよう"],["大凶","今日は何もしないでください"]];
+const result_set = [0,0,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,5,5];
+const colors = ["赤","青","黄","緑","橙","紫","白","黒","紺","金","銀"];
+const items = ["コーヒー","スイーツ","入浴剤","お花","食器","お酒","キッチン用品","アロマ","バッグ","ハンドクリーム","タオル"];
 
 const config = {
     start_page : document.getElementById("start_page"),
@@ -30,6 +35,7 @@ function drawButtonClickListener() {
     // "おみくじを引く" ボタンのクリックイベントの処理...
     displayNone(config.start_page);
     displayBlock(config.result_page);
+    config.start_page.classList.remove("d-flex");//d-flexがあると結果が下に寄ってしまうため。
     config.start_page.innerHTML = "";
     config.result_page.innerHTML = "";
     config.result_page.innerHTML = `
@@ -38,15 +44,15 @@ function drawButtonClickListener() {
             <div class="card m-3 col-3 p-1 set" style="width: 30rem">
                 <div class="card-body">
                     <h5 class="top">おみくじ結果</h5>
-                    <h1 class="result">【大吉】</h1>
-                    <p class="comment">今日はいいことがあるでしょう</p>
+                    <h1 class="result" id="result"></h1>
+                    <p class="comment" id="comment"></p>
                     <div class="element">
                         <p class="item">ラッキーアイテム</p>
-                        <p class="thing">たぬきの置物</p>
+                        <p class="thing" id="item">たぬきの置物</p>
                     </div>
                     <div class="element">
                         <p class="item">ラッキーカラー</p>
-                        <p class="thing">黄色</p>
+                        <p class="thing" id="color">黄色</p>
                     </div>
                 </div>
             </div>
@@ -58,12 +64,13 @@ function drawButtonClickListener() {
         </div>
     </div>
     `;
-
+    random_result();
     // "戻る" ボタンのイベントリスナーを設定
     buttons.back_button = config.result_page.querySelector("#back_button");
     buttons.back_button.addEventListener("click", function () {
         displayNone(config.result_page);
         displayBlock(config.start_page);
+        config.start_page.classList.add("d-flex");
         config.result_page.innerHTML = "";
         config.start_page.innerHTML = "";
         config.start_page.innerHTML = `
@@ -81,6 +88,24 @@ function drawButtonClickListener() {
         // "おみくじを引く" ボタンのイベントリスナーを再度設定
         attachDrawButtonListener();
     });
+}
+
+//おみくじ結果の出力
+function random_result(){
+    var result_number = result_set[Math.floor( Math.random()*39 )];
+    var color_number = Math.floor( Math.random()*11 );
+    var item_number = Math.floor( Math.random()*11 );
+
+    var resultPart = document.getElementById("result");
+    var commentPart = document.getElementById("comment");
+    var colorPart = document.getElementById("color");
+    var itemPart = document.getElementById("item");
+
+    resultPart.innerHTML = "【" + results[result_number][0] + "】";
+    commentPart.innerHTML = results[result_number][1];
+    colorPart.innerHTML = colors[color_number];
+    itemPart.innerHTML = items[item_number];
+
 }
 
 // 初期化時に一度だけボタンのイベントリスナーを設定
